55866e8407483dfdaa437c8d2c2fefd22c3ad2e1
MSevryukov <smlog@yandex.ru>
2023-03-24 08:51:23 +0300

    Player Tests Added

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -1,12 +1,14 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
 
     @Test
-    public void shouldSumGenreIfOneGame() {
+    public void shouldPlayGame() {
         GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
@@ -15,9 +17,20 @@ public class PlayerTest {
         player.play(game, 3);
 
         int expected = 3;
-        int actual = player.sumGenre(game.getGenre());
+        int actual = player.play(game, 0);
         assertEquals(expected, actual);
     }
 
-    // другие ваши тесты
+    @Test
+    public void shouldErrorIfNotInstalled() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.play(game, 3);
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            player.play(game, 3);
+        });
+    }
 }
