b7f99f10c01ea6f18d09691a0f28fdfa68813b98
MSevryukov <smlog@yandex.ru>
2023-03-26 12:47:45 +0300

    Install, mostPlayerByGenre Tests Added

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -31,4 +31,37 @@ public class PlayerTest {
             player.play(game, 3);
         });
     }
+
+    @Test
+    public void shouldNotInstallAlreadyInstalled() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Player player = new Player("Fedor");
+
+        player.installGame(game);
+        player.play(game, 3);
+        player.installGame(game);
+
+        int expected = 3;
+        int actual = player.play(game, 0);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMaxPlayedGameByGenre() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Нетология Баттл Онлайн 2", "Аркады");
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+        player.play(game, 3);
+        player.play(game1, 5);
+
+        Game expected = game1;
+        Game actual = player.mostPlayerByGenre("Аркады");
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
